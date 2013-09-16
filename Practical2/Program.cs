421cb73f9872f0087bc7ca96f8096c87decd35f6
omgjens <jensvanlangen@gmail.com>
2013-09-16 14:09:59 +0200

    Initial commit

---
+++ Practical2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.CV.GPU;
+using Emgu.CV.Util;
+
+namespace Practical2
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new CamCapture.CamCapture());
+        }
+    }
+}

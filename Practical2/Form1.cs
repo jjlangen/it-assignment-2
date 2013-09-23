e551b90696bb4576134b96b4b039ffb998a3289c
omgjens <jlangen>
2013-09-23 21:53:43 +0200

    Added comments

--- Practical2/Form1.cs
+++ Practical2/Form1.cs
@@ -33,7 +33,7 @@ namespace Practical2
         int gs2 = 5;
         int gs3 = 2;
         int gs4 = 2;
-        int cx, cy, x1, y1;
+        int cx, cy, x1, y1, x2, y2;
 
         Rectangle[] rect = new Rectangle[4];
 
@@ -317,8 +317,8 @@ namespace Practical2
         }
 
 
-        // Converts a two-dimensional array
-        public static TOutput[,] ConvertAll<TInput, TOutput>(TInput[,] arr, Converter<TInput, TOutput> conv)
+        // Converts a two-dimensional array.
+        private static TOutput[,] ConvertAll<TInput, TOutput>(TInput[,] arr, Converter<TInput, TOutput> conv)
         {
             int h = arr.GetLength(0);
             int w = arr.GetLength(1);
@@ -331,66 +331,72 @@ namespace Practical2
             return locarr;
         }
 
+        // Computes a 3D histogram from an image
         private void trainingStage()
         {
-            // HS space
+            // Convert the input image from RGB to HSV space
             Image<Bgr, Byte> origImg = new Image<Bgr, Byte>(openFile.FileName);
             Image<Bgr, Byte> smoothImg = origImg.SmoothGaussian(gs1, gs2, gs3, gs4);
             Image<Hsv, Byte> hsvImg = smoothImg.Convert<Hsv, Byte>();
-                        
-            // 3D histogram
+
+            // Declare the 2 dimensional array for the 3D histogram
             double[,] hist3D = new double[10, 10];
 
+            // Scan every pixel in the image
             for (int x = 0; x < hsvImg.Width; x++)
             {
                 for (int y = 0; y < hsvImg.Height; y++)
                 {
+                    // Normalize the Hue and Saturation
                     double h = Math.Round(hsvImg.Data[y, x, 0] / 255d, 1);
-                    h = (h == 0)? 0 : ((h - 0.1) * 10d);
-                                
                     double s = Math.Round(hsvImg.Data[y, x, 1] / 255d, 1);
-                    s = (s == 0)? 0 : ((s - 0.1) * 10d);
-                                
+
+                    // Convert the normalized Hue and Saturation to input for the indices of the hist3D array
+                    h = (h == 0) ? 0 : ((h - 0.1) * 10d);
+                    s = (s == 0) ? 0 : ((s - 0.1) * 10d);
+
+                    // Increment the value at the specific hue, saturation by 1
                     hist3D[(int)h, (int)s]++;
                 }
             }
 
-            // Normalization
+            // Normalize the 3D histogram by. Divide all values by the maximum value
             double max = hist3D.Cast<double>().Max();
             hist3D = ConvertAll(hist3D, item => item / max);
+
+            // Save the 3D histogram to the file "test.txt"
             WriteOut(hist3D, @"test.txt");
             MessageBox.Show("Three dimensional histogram created");
 
-
-            // save histogram>!
-
-            showImage(hsvImg.ToBitmap());
-
+            // Show the original image
+            showImage(origImg.ToBitmap());
         }
 
-        static void WriteOut(double[,] testValues, string fileName)
+        // Writes a 2 dimensional array to a file
+        private static void WriteOut(double[,] vals, string fileName)
         {
             StreamWriter writer = new StreamWriter(fileName);
-            writer.WriteLine(testValues.GetLength(0));
-            writer.WriteLine(testValues.GetLength(1));
-            for (int i = 0; i < testValues.GetLength(0); i++)
+            writer.WriteLine(vals.GetLength(0));
+            writer.WriteLine(vals.GetLength(1));
+            for (int i = 0; i < vals.GetLength(0); i++)
             {
-                for (int j = 0; j < testValues.GetLength(1); j++)
+                for (int j = 0; j < vals.GetLength(1); j++)
                 {
-                    writer.WriteLine(testValues[i, j]);
+                    writer.WriteLine(vals[i, j]);
                 }
             }
             writer.Close();
         }
 
-        static double[,] ReadIn(string fileName)
+        // Reads a 2 dimensional array from a file
+        private static double[,] ReadIn(string fileName)
         {
             StreamReader reader = new StreamReader(fileName);
-            int width;
-            int height;
-            int.TryParse(reader.ReadLine(), out width);
-            int.TryParse(reader.ReadLine(), out height);
-            double[,] readValues = new double[width, height];
+            int w;
+            int h;
+            int.TryParse(reader.ReadLine(), out w);
+            int.TryParse(reader.ReadLine(), out h);
+            double[,] readValues = new double[w, h];
             for (int i = 0; i < readValues.GetLength(0); i++)
             {
                 for (int j = 0; j < readValues.GetLength(1); j++)
@@ -402,8 +408,10 @@ namespace Practical2
             return readValues;
         }
 
+        // Detects the cropped marker saved in "test.txt"
         private void marker_Detection(object sender, EventArgs e)
         {
+            // Convert the captured webcam image from RGB to HSV space
             Image<Bgr, Byte> origImg = capture.RetrieveBgrFrame();
             Image<Bgr, Byte> smoothImg = origImg.SmoothGaussian(gs1, gs2, gs3, gs4);
             Image<Hsv, Byte> hsvImg = smoothImg.Convert<Hsv, Byte>();
@@ -415,6 +423,7 @@ namespace Practical2
             double[] histY = new double[hsvImg.Height];
             int scanx1, scanx2, scany1, scany2;
 
+            // Loop if the marker is detected
             if (markerdetected)
             {
                 scanx1 = cx - ((cx - x1) * 3);
@@ -440,6 +449,7 @@ namespace Practical2
                     }
                 }
             }
+            // Loop if the marker is not detected yet
             else
             {
                 for (int x = 0; x < hsvImg.Width; x++)
@@ -464,18 +474,22 @@ namespace Practical2
                 }
             }
 
+            // Find out where the marker starts and ends
             x1 = Array.FindIndex(histX, item => item > 0);
             y1 = Array.FindIndex(histY, item => item > 0);
-            int x2 = Array.FindLastIndex(histX, item => item > 0);
-            int y2 = Array.FindLastIndex(histY, item => item > 0);
+            x2 = Array.FindLastIndex(histX, item => item > 0);
+            y2 = Array.FindLastIndex(histY, item => item > 0);
+
+            // Find the center of the marker
             cx = ((x2 - x1) / 2 + x1);
             cy = ((y2 - y1) / 2 + y1);
 
+            // Draw the marker and the image
             origImg.Draw(new Ellipse(new PointF(cx, cy), new SizeF((y2 - y1), (x2 - x1)), 360f), new Bgr(0, 0, 0), -1);
-
             showImage(origImg.ToBitmap());
         }
 
+        // Calculates the value for a var in a histogram
         private double gaussian(double value, double[] hist)
         {
             hist = Array.ConvertAll(hist, item => item / img.Width);
